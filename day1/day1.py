953314a689dd1172ed493dd4470cea99bb43aac6
moideen <moideen.kalladi@gmail.com>
2022-12-01 13:28:28 +0530

    black++

--- day1/day1.py
+++ day1/day1.py
@@ -3,7 +3,7 @@
 
 # open and read input file
 with open("input.txt", "r") as f:
-	lines = f.read().split("\n")
+    lines = f.read().split("\n")
 
 # list to hold calorie counts and integer to hold current count
 cals = []
@@ -11,11 +11,11 @@ current_cals = 0
 
 # loop through and count up calories
 for line in lines:
-	if line == "":
-		cals.append(current_cals)
-		current_cals = 0
-	else:
-		current_cals += int(line)
+    if line == "":
+        cals.append(current_cals)
+        current_cals = 0
+    else:
+        current_cals += int(line)
 
 # part 1 - print maximum calories
 print(f"Maximum calories: {max(cals)}")
